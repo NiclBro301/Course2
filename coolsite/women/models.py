2e5962fb7fc0a8044c6072887ec402a90b97f411
NiclBro301 <nickbro1234789@gmail.com>
2023-11-08 09:54:06 +0300

    ДЗ: 1.создать таблицу любимых книг с полем их стоимости в издательстве

--- coolsite/women/models.py
+++ coolsite/women/models.py
@@ -1,12 +1,16 @@
 from django.db import models
+import datetime
 
 #А оно для хранения моделей для представления данных из базы данных
 # Create your models here.
 
+dt_now = datetime.datetime.now()
 
 class Students(models.Model):
     fio = models.CharField(max_length=30)
     year = models.TextField(blank=True)
+    birthday = models.DateField(default=dt_now)
     time_create = models.DateTimeField(auto_now_add=True)
     time_update = models.DateTimeField(auto_now=True)
     is_money = models.BooleanField(default=True)
+
